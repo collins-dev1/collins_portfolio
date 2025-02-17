84d124396405ffcf6d6156fd1b563d7d05c0fb3b
collins-dev1 <collinsxuccessful39@gmail.com>
2025-02-17 22:24:25 +0100

    cert

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -363,7 +363,7 @@ https://www.margaretwhitlock.com/ -->
                 </div>
                 {{-- my certificate --}}
 
-                <div class="d-flex flex-column justify-content center align-items-center px-5 mt-3">
+                <div class="d-flex flex-column justify-content center align-items-center mt-3">
                     <img src="{{asset('collins_cv/my_maincert.jpg')}}" alt="" width="100%" height="300px" class="object-fit-contain">
                 </div>
                 <div class="d-flex justify-content-center align-items-center mt-3">

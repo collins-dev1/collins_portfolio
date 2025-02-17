903e8d3b4953a1cd650b58f139c6a3a5441cb124
collins-dev1 <collinsxuccessful39@gmail.com>
2025-02-17 22:20:45 +0100

    kodex

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -321,7 +321,7 @@ https://www.margaretwhitlock.com/ -->
                                 </h4>
                                 <h4>Computer Craft Work</h4>
                                 <h5>2021 - 2023</h5>
-                                <h4>FULL-STACK WEB DEVELOPMENT PROGRAM -> Kodex Limited</h4>
+                                <h4>FULL-STACK WEB DEVELOPMENT PROGRAM -> Kodex Limited <a href="https://kodexafrica.com/" target="_blank">www.kodexafrica.com</a></h4>
                                 <h5>2023</h5>
                             </div>
                         </div>
@@ -364,7 +364,7 @@ https://www.margaretwhitlock.com/ -->
                 {{-- my certificate --}}
 
                 <div class="d-flex flex-column justify-content center align-items-center px-5 mt-3">
-                    <img src="{{asset('collins_cv/my_maincert.jpg')}}" alt="" width="80%" height="300px" class="object-fit-cover">
+                    <img src="{{asset('collins_cv/my_maincert.jpg')}}" alt="" width="100%" height="300px" class="object-fit-contain">
                 </div>
                 <div class="d-flex justify-content-center align-items-center mt-3">
                     <a href="{{ asset('collins_cv/my_resume.pdf') }}" download="Collins Resume"
